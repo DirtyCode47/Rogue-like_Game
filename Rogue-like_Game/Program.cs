3d3324d45c5ffc89b49e74d8437d09ce81522083
DirtyCode47 <alexprog47@mail.ru>
2023-12-30 17:38:07 +1000

    Initial project

---
+++ Rogue-like_Game/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Rogue_like_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
